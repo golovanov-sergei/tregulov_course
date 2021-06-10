93907d1ded34415a900c0e612001a9879e6446ea
imp <imp.sergei@gmail.com>
2021-06-10 17:17:48 +0300

    DI - setter

--- src/main/java/com/tregulov/spring/introduction/Person.java
+++ src/main/java/com/tregulov/spring/introduction/Person.java
@@ -3,8 +3,17 @@ package com.tregulov.spring.introduction;
 public class Person {
     private Pet pet;
 
+    public Person() {
+        System.out.println("Clear person created!");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Person set pet");
+        this.pet = pet;
+    }
+
     public Person(Pet pet) {
-        System.out.println("Person created!");
+        System.out.println("Person with pet created!");
         this.pet = pet;
     }
     public void callPet(){
