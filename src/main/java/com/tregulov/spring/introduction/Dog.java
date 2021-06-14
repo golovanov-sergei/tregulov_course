f52186d0d658d58fd9d169bf62860352e2b213b7
Sergei <imp.sergei@gmail.com>
2021-06-14 21:23:40 +0300

    20. @Scope @PostConstruct @PreDestroy

--- src/main/java/com/tregulov/spring/introduction/Dog.java
+++ src/main/java/com/tregulov/spring/introduction/Dog.java
@@ -3,18 +3,22 @@ package com.tregulov.spring.introduction;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component("dogBean")
-@Scope("prototype")
 
 public class Dog implements Pet{
     public Dog() {
         System.out.println("Dog created!");
     }
 
+    @PostConstruct
     public void init(){
         System.out.println("Dog init method");
     }
 
+    @PreDestroy
     public void destroy(){
         System.out.println("Dog destroy method");
     }
--- src/main/java/com/tregulov/spring/introduction/ScopeTest.java
+++ src/main/java/com/tregulov/spring/introduction/ScopeTest.java
@@ -6,10 +6,13 @@ public class ScopeTest {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
         Dog dog1 = context.getBean("dogBean", Dog.class);
+        dog1.say();
+/*
         Dog dog2 = context.getBean("dogBean", Dog.class);
         System.out.println(dog1.equals(dog2));
         System.out.println(dog1);
         System.out.println(dog2);
+*/
 
         context.close();
     }
