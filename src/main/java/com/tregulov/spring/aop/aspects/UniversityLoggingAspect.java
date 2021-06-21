3007575156e221fdd198b586b6264446e55bb2b5
Sergei <imp.sergei@gmail.com>
2021-06-21 20:49:13 +0300

    32. @AfterReturning

--- src/main/java/com/tregulov/spring/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/com/tregulov/spring/aop/aspects/UniversityLoggingAspect.java
@@ -1,9 +1,12 @@
 package com.tregulov.spring.aop.aspects;
 
+import com.tregulov.spring.aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Aspect
@@ -13,9 +16,19 @@ public class UniversityLoggingAspect {
         System.out.println("beforeGetStudentsLoggingAdvice: логгируем получение" +
                 "списка студентов перед методом getStudents");
     }
-    @AfterReturning("execution(* getStudents(..))")
-    public void afterReturningGetStudentsLoggingAdvice(){
-        System.out.println("afterReturningGetStudentsLoggingAdvice: логгируем получение" +
+    @AfterReturning(pointcut = "execution(* getStudents(..))", returning = "students")
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+        Student firstStudent = students.get(0);
+
+        String nameSirname = firstStudent.getNameSurname();
+        nameSirname = "Mr. "+nameSirname;
+        firstStudent.setNameSurname(nameSirname);
+
+        double avgGrade = firstStudent.getAvgGrade();
+        avgGrade = avgGrade+1;
+        firstStudent.setAvgGrade(avgGrade);
+
+        System.out.println("afterReturningGetStudentsLoggingAdvice: логгируем получение " +
                 "списка студентов после метода getStudents");
     }
 }
