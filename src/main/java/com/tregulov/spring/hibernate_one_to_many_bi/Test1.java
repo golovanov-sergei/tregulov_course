904e0cf98bff65646bd74b052542ec6078c3cf17
imp <imp.sergei@gmail.com>
2021-06-25 15:11:51 +0300

    52. Hibernate FETCH type

--- src/main/java/com/tregulov/spring/hibernate_one_to_many_bi/Test1.java
+++ src/main/java/com/tregulov/spring/hibernate_one_to_many_bi/Test1.java
@@ -17,29 +17,38 @@ public class Test1 {
         Session session = null;
 
         try {
-//            session = factory.getCurrentSession();
-//            Department dep = new Department("IT", 500,700);
-//            Employee emp1 = new Employee("Shock","Imp", 700);
-//            Employee emp2 = new Employee("Elena","Smirnova", 500);
-//            dep.addEplyeeToDepartment(emp1);
-//            dep.addEplyeeToDepartment(emp2);
-//
-//            session.beginTransaction();
-//            session.save(dep);
-//
-//
-//            session.getTransaction().commit();
-//            System.out.println("Done!");
+/*
+            session = factory.getCurrentSession();
+            Department dep = new Department("IT", 500,700);
+            Employee emp1 = new Employee("Shock","Imp", 700);
+            Employee emp2 = new Employee("Elena","Smirnova", 500);
+            Employee emp3 = new Employee("W3","Sm3", 550);
+            Employee emp4 = new Employee("W4","Sm4", 600);
+            dep.addEplyeeToDepartment(emp1);
+            dep.addEplyeeToDepartment(emp2);
+            dep.addEplyeeToDepartment(emp3);
+            dep.addEplyeeToDepartment(emp4);
 
+            session.beginTransaction();
+            session.save(dep);
+
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+*/
+//************************************************************
             session = factory.getCurrentSession();
 
             session.beginTransaction();
-//            Department department = session.get(Department.class,1);
-//            System.out.println(department);
-//            System.out.println(department.getEmps());
+            System.out.println("Get department");
+            Department department = session.get(Department.class,3);
+            System.out.println("Print department");
+            System.out.println(department);
+            System.out.println("Print employees");
+            System.out.println(department.getEmps());
 
-            Employee employee = session.get(Employee.class,2);
-            session.delete(employee);
+//            Employee employee = session.get(Employee.class,3);
+//            session.delete(employee);
 
 
             session.getTransaction().commit();
--- src/main/java/com/tregulov/spring/hibernate_one_to_many_bi/entity/Employee.java
+++ src/main/java/com/tregulov/spring/hibernate_one_to_many_bi/entity/Employee.java
@@ -20,8 +20,8 @@ public class Employee {
     @Column(name = "salary")
     private int salary;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.MERGE})
-//    @ManyToOne(cascade = CascadeType.ALL)
+//    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.MERGE})
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "department_id")
     private Department department;
 
